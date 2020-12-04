366d694e6e469cdc3255a6fe73f87c716421a6fc
sebdisdv <seba.dissegna@gmail.com>
2020-12-04 20:38:07 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,6 @@
+
 <template>
-<<<<<<< HEAD
-  <v-app>
-    <v-app-bar app color="#81D4FA">
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo.png"
-          transition="scale-transition"
-          width="100"
-        />
-
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Login</span>
-        <v-icon>mdi-account-circle</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <br>
-      
-      <v-container align-content-lg >
-        
-            <v-row   class="signup-style">
-              <v-col>
-                <AboutUs/>
-              </v-col>
-              
-              <v-col>
-                <v-card elevation-5 class="signup-form-style" title="SIGN UP">
-                  <SignupForm/>
-                </v-card>
-              </v-col>
-            </v-row>
-             
-      </v-container>
-       
-    </v-main>
-  </v-app>
-=======
     <router-view><Home/></router-view>
->>>>>>> 4f050ccb8e5aa6d8dae537399b7a406e30cfa034
 </template>
 
 <script>
