bea028a1880eeddab345063b8b2bfe0ce7270ad4
sebdisdv <seba.dissegna@gmail.com>
2020-12-04 11:43:55 +0100

    added store

---
+++ src/components/LogInForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "LogInForm"
+})
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import vuex from 'vuex'
+
 
 Vue.config.productionTip = false
+Vue.use(vuex)
+
+const session_store = new vuex.Store({
+    state: {
+        user_email: "",
+        user_name: ""
+    }
+})
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    router,
+    vuetify,
+    store: session_store,
+    render: h => h(App)
 }).$mount('#app')
