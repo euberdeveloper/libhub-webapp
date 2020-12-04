7c4ae632abdfcc5e03f59d0de920bb2694eef2c5
sebdisdv <seba.dissegna@gmail.com>
2020-12-04 20:41:14 +0100

    removed unnecessary password rule

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -72,7 +72,6 @@ export default {
         passwordRules: {
           required: value => !!value || 'Required.',
           min: v => v.length >= 8 || 'Min 8 characters',
-          emailMatch: () => (`The email and password you entered don't match`),
         },
         name: '',
         nameRules: [
