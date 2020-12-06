8b56ceda89bbd09272969d6f54a7fff23eef734f
sebdisdv <seba.dissegna@gmail.com>
2020-12-06 17:17:07 +0100

    added Error page

--- src/router/index.js
+++ src/router/index.js
@@ -2,35 +2,45 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/default/Home.vue'
 import HomeBar from '../views/bars/HomeBar.vue'
+import ErrorPage from '../views/default/Error_page.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    components:{
-      default: Home,
-      bar: HomeBar
-    } 
-  },
-  {
-    path: '/dash/:id',
-    name: 'Dashboard',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    components: {
-      default: () => import(/* webpackChunkName: "dashboard" */ '../views/default/Dashboard.vue'),
-      bar: () => import(/* webpackChunkName: "dashboard" */ '../views/bars/AppBar.vue'),
+const routes = [{
+        path: '/',
+        name: 'Home',
+        components: {
+            default: Home,
+            bar: HomeBar
+        }
+    },
+    {
+        path: '/dash/:id',
+        name: 'Dashboard',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        components: {
+            default: () =>
+                import ( /* webpackChunkName: "dashboard" */ '../views/default/Dashboard.vue'),
+            bar: () =>
+                import ( /* webpackChunkName: "dashboard" */ '../views/bars/AppBar.vue'),
+        }
+    },
+    {
+        path: '/error_page',
+        name: 'Error_page',
+        components: {
+            default: ErrorPage,
+            bar: HomeBar
+        }
     }
-  }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/default/Error_page.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <div >
+      <v-row class="central-align-style">
+        <v-col style="align-contents: center">
+          <h1 style="color: #FF1744">Something went wrong</h1>
+          <v-img
+            src="../../assets/doggo.png"
+            max-height="500"
+            max-width="500"
+          ></v-img>
+        </v-col>
+        <v-col>
+          <v-btn color="error" x-large tile @click="send_to_home" elevation="0">Return Home</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Error_page",
+  methods: {
+    send_to_home() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
