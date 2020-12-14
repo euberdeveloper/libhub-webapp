6dc0a450403b5e4b0a97f8e88161ad55e2095758
x3n0peltis <federico.stefini@studenti.unitn.it>
2020-12-14 03:18:10 +0100

    friendview

--- src/views/default/Friends.vue
+++ src/views/default/Friends.vue
@@ -1,15 +1,39 @@
 <template>
-  <v-container fluid>
-    <freinds-tabs/>
+  <v-container fluid class="d-flex">
+    <friends-tabs />
+    
+    <v-row>
+      <v-dialog
+        v-model="showChatDialog"
+        :key="getChatId"
+        fullscreen
+        hide-overlay
+      >
+        <chat-messages />
+      </v-dialog>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import FreindsTabs from '@/components/FreindsTabs.vue';
+import FriendsTabs from "@/components/FriendsTabs.vue";
+import ChatMessages from "@/components/ChatMessages.vue";
+
 export default {
-  components: { FreindsTabs },
   name: "Friends",
-  data: () => ({
-  }),
+  components: { FriendsTabs, ChatMessages },
+  data: () => ({}),
+  computed: {
+    showChatDialog: {
+      get: function () {
+        return this.$store.state.chatDialog;
+      },
+    },
+    getChatId: {
+      get: function () {
+        return this.$store.getters.getChatId;
+      },
+    },
+  },
 };
 </script>
