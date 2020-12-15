1547dc0edcd935c9e3ddab383a3db316e8883067
x3n0peltis <federico.stefini@studenti.unitn.it>
2020-12-15 02:40:59 +0100

    store refactor

--- src/store/index.js
+++ src/store/index.js
@@ -4,38 +4,87 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        UserName: "",
-        UserEmail: "",
-        UserId: "",
+        user: JSON.parse(localStorage.getItem("user")) || null,
         LibraryId: "",
+        ChatID: "",
         BookId: "",
-        error_message: "ciao sono un messaggio di errore generico che può essere modificato :)",
+        model: null,
+        tabs: [true, false, false],
+        error_message: "",
         isbn_obtained_from_image: "",
         loginDialog: false,
+        libraryDialog: false,
+        bookDialog: false,
         InsertBookForm_Dialog: false,
         InsertLibraryForm_Dialog: false,
-        libraryDialog: false,
+        chatDialog: false,
+        friend: {},
         libraries: [],
         books: [],
+        book: {},
+        chat: {},
+        chatReceiver: "",
     },
     mutations: {
+        //Setters
+        clearStore(state) {
+            state.user = null
+            state.LibraryId = ""
+            state.ChatID = ""
+            state.BookId = ""
+            state.model = null
+            state.tabs = [true, false, false]
+            state.error_message = ""
+            state.isbn_obtained_from_image = ""
+            state.loginDialog = false
+            state.libraryDialog = false
+            state.bookDialog = false
+            state.InsertBookForm_Dialog = false
+            state.InsertLibraryForm_Dialog = false
+            state.ChatForm = false
+            state.libraries = []
+            state.books = []
+            state.book = {}
+            state.chat = {}
+            state.chatReceiver = ""
+        },
+        setUser(state, value){
+            state.user = value;
+        },
+        setLibraryId(state, value) {
+            state.LibraryId = value
+        },
+        setBookId(state, value) {
+            state.BookId = value
+        },
+        setErrorMessage(state, value) {
+            state.error_message = value
+        },
+        setIsbnObatinedFromImage(state, value) {
+            state.isbn_obtained_from_image = value
+        },
+        setLibraries(state, value) {
+            state.libraries = value
+        },
+        addLibrary(state, library) {
+            state.libraries.push(library)
+        },
+        setBooks(state, value) {
+            state.books = value
+        },
+        addBookToBooks(state, book) {
+            state.books.push(book)
+        },
+        setBook(state, book) {
+            state.book = book
+        },
+
+        //show-hide Dialogs
         showLoginDialog(state) {
             state.loginDialog = true
         },
         hideLoginDialog(state) {
             state.loginDialog = false
-        },
-        setUserId(state, value) {
-            state.UserId = value;
-        },
-        setLibraryId(state, value) {
-            state.LibraryId = value;
-        },
-        setErrorMessage(state, value) {
-            state.error_message = value;
-        },
-        setIsbnObatinedFromImage(state, value) {
-            state.isbn_obtained_from_image = value
         },
         show_InsertBookForm_Dialog(state) {
             state.InsertBookForm_Dialog = true
@@ -49,25 +98,61 @@ export default new Vuex.Store({
         hide_InsertLibraryForm_Dialog(state) {
             state.InsertLibraryForm_Dialog = false
         },
-        setLibraries(state, value){
-            state.libraries = value;
+        show_LibraryDetails_Dialog(state, lid) {
+            if (lid != null)
+                state.LibraryId = lid
+            state.libraryDialog = true
         },
-        addLibrary(state, library){
-            state.libraries.push(library);
+        hide_LibraryDetails_Dialog(state) {
+            state.libraryDialog = false
         },
-        setBooks(state, value){
-            state.books = value;
+        show_BookDetails_Dialog(state, bid) {
+            if (bid != null)
+                state.BookId = bid
+            state.bookDialog = true
         },
-        addBook(state, book){
-            state.books.push(book);
+        hide_BookDetails_Dialog(state) {
+            state.bookDialog = false
         },
-        show_LibraryDetails_Dialog(state, lid){
-            if(lid != null)
-                state.LibraryId = lid;
-            state.libraryDialog = true;
+        showChatDialog(state, data) {
+            state.ChatID = data.chatId
+            state.friend = data.friend
+            state.chatDialog = true
         },
-        hide_LibraryDetails_Dialog(state){
-            state.libraryDialog = false;
+        hideChatDialog(state) {
+            state.chatDialog = false
+        },
+    },
+    getters: {
+        getUser: (state) => {
+            return state.user
+        },
+        getFriend: (state) => {
+            return state.friend
+        },
+        getLibraries: (state) => {
+            return state.libraries
+        },
+        getLibraryId: (state) => {
+            return state.LibraryId
+        },
+        getBooks: (state) => {
+            return state.books
+        },
+        getBookId: (state) => {
+            return state.BookId
+        },
+        getBook: (state) => {
+            return state.book
+        },
+        getChat: (state) => {
+            return state.chat
+        },
+        getChatId: (state) => {
+            return state.ChatID
+        },
+        getChatReceiver: (state) => {
+            return state.chatReceiver
         }
     },
     actions: {},
